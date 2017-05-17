70e43ddce913d610ff306ea757f7518940755194
Clear Tax <cleartax@Concurs-MacBook-Pro.local>
2017-05-17 14:46:36 +0530

    status and header config

--- src/worker.base.js
+++ src/worker.base.js
@@ -6,13 +6,13 @@
   })
   self.addEventListener('fetch',function(event){
     var route = event.request.url.replace(event.target.location.origin,"");
-    var init = {
-      "status" : 200,
-      "headers" : {
-        "Content-Type" : 'application/json'
-      }
-    }
     if(route && apis[route]){
+      var init = {
+        "status" : apis[route].status || 200,
+        "headers" : apis[route].headers || {
+          "Content-Type" : 'application/json'
+        }
+      }
       event.respondWith(new Response(JSON.stringify(apis[route].response),init))
     }
     return fetch(request);
